c12fd7f900f7684614ecf6ec178c92fddfe0b6e9
Resil kv <resil@quantumnexlabs.com>
2025-10-15 16:21:03 +0530

    middleware added

--- app/main.py
+++ app/main.py
@@ -10,8 +10,22 @@ from app.api.analytics import router as analytic_router
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from fastapi import status
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 app.include_router(quiz_router)
